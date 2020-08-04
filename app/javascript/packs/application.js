cfa64461e6ab9c9dfdb9698eb4a016a76cfb0795
Dat Tran <datga2006@gmail.com>
2020-08-04 11:49:35 +0700

    Update landing page continue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,9 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
- 
+import "@fortawesome/fontawesome-free/js/all";
+
+
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
