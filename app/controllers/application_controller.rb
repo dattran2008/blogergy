fdc061318d95b87cd4b6b6329225dc6de67e0d27
Dat Tran <datga2006@gmail.com>
2020-08-07 17:02:11 +0700

    Update authentication & session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
-    
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    #memoization
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    #need a bolean to check if already login
+    !!current_user
+  end
+
+  def require_login
+    if !logged_in?
+      flash[:danger] = "You must login first before you can access!"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 class SessionsController < ApplicationController
-    def new 
+  def new 
+  end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Logged in successfully!'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Your email or password was incorrect!'
+      render 'new'
     end
+  end
 
-    def create
-        
-    end
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out!'
+    redirect_to login_path
+  end
 end
